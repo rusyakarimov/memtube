e1929da21b7db1d2277f93938eff87f1838d979f
rusyakarimov <83639776+rusyakarimov@users.noreply.github.com>
2022-01-15 00:04:45 +0300

    add Template class

---
+++ classes/Template.php
@@ -0,0 +1,22 @@
+<?php
+class Template
+{
+    /*
+    * $name - name of template
+    * array $data - some data in template
+    */
+    public function include_template($name, array $data = [])
+    {
+        $name = 'templates/' . $name;
+
+        if (!is_readable($name)) {
+            return 'Error';
+        }
+
+        ob_start();
+        extract($data);
+        require_once $name;
+
+        return ob_get_clean();
+    }
+}
