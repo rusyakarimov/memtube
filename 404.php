2fe48a671469a2ad18e76527f4cbb4a29640fccf
rusyakarimov <83639776+rusyakarimov@users.noreply.github.com>
2022-01-26 23:55:28 +0300

    404 change

--- 404.php
+++ 404.php
@@ -1,17 +1,19 @@
 <?php
 require_once ROOT_DIR . '/inc/config.php';
-$title = "Страницы не существует!";
-require_once ROOT_DIR . '/inc/header.php'; ?>
-
-<div class="content">
-    <?php require_once ROOT_DIR . '/inc/sidebar.php'; ?>
-
-    <main class="content__main">
-        <h2 class="content__main-heading"><?= $title ?></h2>
-
-        <p class="error-message">Извините, запрашиваемой страницы не существует!</p>
-
-    </main>
-</div>
+$title = "MEM-TUBE: Просмотр категории";
+require_once ROOT_DIR . '/inc/header.php';
+require_once ROOT_DIR . '/inc/connect.php';
+?>
+<main>
+    <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <h1 class="fw-light">Ошибка 404</h1>
+                <h1 class="fw-light">Страницы не существует</h1>
+                <p>
+            </div>
+        </div>
+    </section>
+</main>
 
 <?php require_once ROOT_DIR . '/inc/footer.php'; ?>
